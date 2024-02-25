a4fe799b8dbda9a8e827361e4d4958fabdb5eb99
traginlaw <traginlaw@gmail.com>
2024-02-24 18:31:20 -0600

    finally done

--- script.js
+++ script.js
@@ -85,171 +85,64 @@ const fruit = [
 
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
-// const searchContainer = document.querySelector('.searchContainer');
-
-function removeVowels(str) {
-	const vowels = 'aeiou';
-	return str
-		.toLowerCase()
-		.split('')
-		.filter(function (val) {
-			return vowels.indexOf(val) === -1;
-		});
-	// .join('');
-}
 
 function search(str) {
 	let results = [];
-
-	if (str.length > -1) {
+	if (str.length !== -1) {
 		results = fruit.filter((inputVal) =>
 			inputVal.toLowerCase().includes(str.toLowerCase())
 		);
-		// results.classList.add('suggestions');
+		showSuggestions(results, str);
 	}
-	showSuggestions(results, str);
 
-	// showSuggestions(results, inputVal);
-	// results.classList.add('suggestions ul');
-
-	//console.log(classList);
-	// //showSuggestions();
+	
 }
-
-// 	return fruit.filter((val) => {
-// 		return (results = fruit.includes(val.toLowerCase()));
-// 	});
-
-// for Each char of str) {
-// let newL = document.createElement('li')
-
-// console.log(results);
-// return results;
 
 function searchHandler(e) {
 	let str = e.target.value;
 	search(str);
-	// showSuggestions(search(str));
-
-	// let results = [];
-	// let input = e.target.value;
-	// fruit.forEach(
-	// 	(results = fruit.filter((input) => {
-	// 		return fruit.includes(input.toLowerCase());
-	// 	}))
-	// );
+	
 }
 
 function showSuggestions(results, inputVal) {
-	suggestions.innerHTML = results.map((i) => `<li>${i}</li>`).join('');
+	suggestions.innerHTML = '';
+	const newExp = new RegExp(inputVal, 'ig');
+
+	if (inputVal.length > 0) {
+		results.forEach((keyword) => {
+			const newL = document.createElement('li');
+			newL.innerHTML = keyword.replace(newExp, '<b>$&</b>');
+			suggestions.appendChild(newL);
+
+			newL.addEventListener('click', () => {
+				input.value() = keyword;
+				suggestions.classList.remove('has-suggestions');
+			});
+		});
+		suggestions.classList.add('has-suggestions');
+	} else {
+		suggestions.classList.remove('has-suggestions');
+	}
 }
-// const text = results.map((list) => {
-// 	return `<li> ${list} </li>`;
-// });
-// suggestions.innerHTML = `<`;
 
-// let li = document.createElement('li');
+function useSuggestion(e) {
+	console.log(e);
+	if (e.target.tagName === 'LI') {
+		input.value = e.target.textContent;
+		suggestions.innerHTML = '';
+	}
+}
 
-// li.innerHTML =
-// newL = document.getElementsByTagName('ul');
-// newL.innerHTML = results
-// 	.map((list) => {
-// 		return newL.appendChild(`<li>${search(results)} </li>`);
-// 	})
-// 	.join('');
-
-// if (search() && searchHandler()) {
-// 	console.log(results);
-// }
-// console.log(inputVal);
-// if (inputVal.length > -1) {
-// 	results = search().value;
-// 	for (let i = 0; i > results.length; i++) {
-// 		console.log(results);
-
-// 	}
-// }
-// console.log(results);
-
-function useSuggestion(e) {}
+function color(e) {
+	const newItem = suggestions.querySelectorAll('li')
+	newItem.forEach(item => {
+		item.classList.remove('highlight')
+	})
+	if(e.target.tagName === 'LI') {
+		e.target.classList.add('highlight')
+	}
+}
 
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
-
-// let results = [];
-// if (str.length) {
-// 	console.log(str);
-// 	str.toLowerCase();
-// 	results = fruit.filter((el) => {
-// 		return fruit.toString().includes(el);
-// 	});
-// 	return results;
-// }
-
-// results = fruit.filter().includes(str);
-
-// results.includes(inputVal.toLowerCase());
-// search();
-// let results = [];
-// let inputVal = searchContainer.value;
-// if (inputVal.length) {
-// 	fruit.filter((inputVal) => {
-// 		return inputVal.toLowerCase().includes(inputVal.toLowerCase());
-// 	});
-// 	console.log(results);
-// 	return results;
-// }
-
-// 		results = fruit.filter(function (value) {
-// 			fruit.includes(value);
-// 		});
-// 		console.log(results);
-// 		console.log(value);
-// 	}
-// }
-
-// 	for(const fruit of results) {
-// 		const listResults = document.createElement('li')
-// 		listResults.classList.add('listResults')
-// 		const text = document.createTextNode(fruit)
-// 		listResults.appendChild(text)
-// 		suggestions.appendChild(listResults)
-// 	}
-
-// }
-// let value = e.target.value
-// if(value && value.length > 0) {
-// 	value = value.toLowerCase()
-
-// }
-
-// function noResults() {
-// 	const error = document.createElement('li')
-// 	error.classList
-//}
-// 	let userInput = e.target.value;
-// 	let emptyArray = [];
-// 	if (userInput) {
-// 		emptyArray = fruit.filter((data) => {
-// 			return data.toLowerCase().includes(userInput.toLowerCase());
-// 		});
-// 		emptyArray = emptyArray.map((data) => {
-// 			return (data = '<li>' + data + '<li>');
-// 		});
-// 		input.classList.add('.suggestions');
-// 		showSuggestions(emptyArray);
-// 	}
-// }
-
-//search handler should
-
-// console.log(str);
-
-// console.log(e);
-// console.log(fruit.toString().includes('ap'));
-// const newStr = fruit.toString();
-// console.log(newStr);
-// const search = fruit.filter(() => {
-// 	fruit.includes('ap');
-// 	console.log(search);
-// 	return search;
+suggestions, addEventListener('mouseover', color);
